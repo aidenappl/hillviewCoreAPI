26ae6ae25bf8ecc10b8bee3a7967b095ebdbd688
Aiden Appleby <aiden.appleby3@gmail.com>
2023-04-16 21:41:18 -0400

    added removing and adding videos to playlist

--- query/updatePlaylist.query.go
+++ query/updatePlaylist.query.go
@@ -14,14 +14,18 @@ type UpdatePlaylistRequest struct {
 }
 
 type UpdatePlaylistChanges struct {
-	Name        *string `json:"name"`
-	Status      *int    `json:"status"`
-	Description *string `json:"description"`
-	BannerImage *string `json:"banner_image"`
-	Route       *string `json:"route"`
+	Name         *string `json:"name"`
+	Status       *int    `json:"status"`
+	Description  *string `json:"description"`
+	BannerImage  *string `json:"banner_image"`
+	Route        *string `json:"route"`
+	RemoveVideos *[]int  `json:"remove_videos"`
+	AddVideos    *[]int  `json:"add_videos"`
 }
 
 func UpdatePlaylist(db db.Queryable, req UpdatePlaylistRequest) (*structs.Playlist, error) {
+	changesMade := false
+
 	// check required fields
 	if req.ID == nil {
 		return nil, fmt.Errorf("required field id is nil")
@@ -29,6 +33,48 @@ func UpdatePlaylist(db db.Queryable, req UpdatePlaylistRequest) (*structs.Playli
 
 	if req.Changes == nil {
 		return nil, fmt.Errorf("required field changes is nil")
+	}
+
+	if req.Changes.RemoveVideos != nil {
+		changesMade = true
+		// remove videos
+		for _, videoID := range *req.Changes.RemoveVideos {
+			query, args, err := sq.Delete("playlist_associations").
+				Where(sq.Eq{"playlist_id": *req.ID, "video_id": videoID}).
+				ToSql()
+			if err != nil {
+				return nil, fmt.Errorf("failed to form query: %v", err)
+			}
+
+			_, err = db.Exec(query, args...)
+			if err != nil {
+				return nil, fmt.Errorf("failed to remove video from playlist: %v", err)
+			}
+		}
+	}
+
+	if req.Changes.AddVideos != nil {
+		changesMade = true
+		// add videos
+		for _, videoID := range *req.Changes.AddVideos {
+			query, args, err := sq.Insert("playlist_associations").
+				Columns("playlist_id", "video_id").
+				Values(*req.ID, videoID).
+				ToSql()
+			if err != nil {
+				return nil, fmt.Errorf("failed to form query: %v", err)
+			}
+
+			_, err = db.Exec(query, args...)
+			if err != nil {
+				return nil, fmt.Errorf("failed to add video to playlist: %v", err)
+			}
+		}
+	}
+
+	if changesMade && req.Changes.Name == nil && req.Changes.Status == nil && req.Changes.Description == nil && req.Changes.BannerImage == nil && req.Changes.Route == nil {
+		// if no changes were made to the playlist, just return the playlist
+		return GetPlaylist(db, GetPlaylistRequest{ID: req.ID})
 	}
 
 	// build query
